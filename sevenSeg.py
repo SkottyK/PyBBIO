7ef4ac85c1aab0815a6c6c1ead77359d4f351adb
loveatfirstbyte <skrulcik@gmail.com>
2012-12-10 17:00:01 -0500

    Update sevenSeg.py

--- sevenSeg.py
+++ sevenSeg.py
@@ -1,6 +1,8 @@
 """sevenSegShift.py - Scott K 11/6/12
    Object for use with a seven segment display connected to a shift register
    for use with beagle bone
+
+   *note this is meant to control 1 digit per register, not multiple digit displays
 """
 
 #-----------------------------------------------------------------------------
@@ -10,6 +12,23 @@ from shiftRegister import *
 
 class sevenSegShift(object):
 
-    def _init_(self, ser=8, rclk=9, srclk=10, numSeg=1, abcdefg={0,1,2,3,4,5,6,7}):
-        disp=new shiftRegister(ser, rclk, srclk, numSeg)
+    def _init_(self, ser=8, rclk=9, srclk=10, anodeCathode=1, numDisp=1, abcdefgp={0,1,2,3,4,5,6,7,8}):
+        """Initialized necessary variables for sevenSegShift class"""
+      
+        #creates new shift register object per display
+        for i in range(0, numDisp):
+            disp=new shiftRegister(ser, rclk, srclk)    #creates 
         self.a=abcdefg[0]
+        self.b=abcdefg[1]
+        self.c=abcdefg[2]
+        self.d=abcdefg[3]
+        self.e=abcdefg[4]
+        self.f=abcdefg[5]
+        self.g=abcdefg[6]
+        self.dp=abcdefg[7]
+        if anodeCathode==0:
+            self.ON=HIGH
+            self.OFF=LOW
+        else:
+            self.ON=LOW
+            self.OFF=HIGH
