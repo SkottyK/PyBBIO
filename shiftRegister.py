54b9cdf8772de8f7fa30ec46db91ef5f6e101075
loveatfirstbyte <skrulcik@gmail.com>
2012-12-09 18:46:45 -0800

    Create shiftRegister.py
    Shift Register object, adapted from Bildr 74HC595 tutorial by Scott K

---
+++ shiftRegister.py
@@ -0,0 +1,69 @@
+# shift_test.py - Scott K 11/6/2012
+# 
+# Interfaces with shift registers to create displays.
+# 
+# This example is in the public domain
+from bbio import *
+
+
+#-----------------------------------------------------------------------------
+
+def _init_(self, ser=8, rclk=9, srclk=10, numReg=1):
+    self.SER = ser	    				   	  #pin 14 on the 75HC595
+    self.RCLK = rclk    				 	  #pin 12 on the 75HC595
+    self.SRCLK = srclk  				 	  #pin 11 on the 75HC595
+    
+    self.NUM_REGISTERS=numReg   				  #number of shift registers
+    self.NUM_REG_PINS=NUM_REGISTERS*8                      #number of total pins
+
+    self.registers[NUM_REG_PINS]
+
+
+
+def configure():
+	"""Sets all of the control pins to output and then makes them all LOW"""
+    pinMode(self.SER, OUTPUT)
+    pinMode(self.RCLK, OUTPUT)
+    pinMode(self.SRCLK, OUTPUT)
+
+    #reset all register pins
+    clearRegisters()
+    writeRegisters()
+  
+#-----------------------------------------------------------------------------          
+
+def clearRegisters():
+	"""Makes shift register pins go LOW"""
+    for(int i = NUM_REG_PINS - 1; i >=  0; i--):
+         self.registers[i] = LOW
+
+#-----------------------------------------------------------------------------
+
+def writeRegisters():
+	"""Writes values to all shift register pins after they have been set"""
+    digitalWrite(self.RCLK, LOW)
+  
+    for i in range(self.NUM_REG_PINS):
+        digitalWrite(self.SRCLK, LOW)
+        int val = registers[self.NUM_REG_PINS-1-i]
+        digitalWrite(self.SER, val)
+        digitalWrite(self.SRCLK, HIGH)
+    
+    digitalWrite(self.RCLK, HIGH)
+
+#-----------------------------------------------------------------------------
+
+def setRegisterPin(int index, int value):
+	"""Sets, but does not right the value of a shift register pin"""
+    self.registers[index] = value
+
+#-----------------------------------------------------------------------------
+
+def setPins(pins[], value[]):
+	"""Sets an array of pin numbers to a corresponding value according to a seperate an array"""
+	for i in range(pins):			#cycles through pin numbers
+		newVal=LOW
+		if value[i]:
+			newVal=HIGH
+		setRegisterPin(pins[i], newVal)  #sets pin to the value at a corresponding value of the second array
+	writeRegisters()  					#physically changes pin values
