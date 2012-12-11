378a0d4a91d816412cf480c8593d4388e233febd
loveatfirstbyte <skrulcik@gmail.com>
2012-12-11 10:23:49 -0500

    Update sevenSeg.py

--- sevenSeg.py
+++ sevenSeg.py
@@ -12,13 +12,15 @@ from shiftRegister import *
 
 class sevenSegShift(object):
 
+    
+
+
     def _init_(self, ser=8, rclk=9, srclk=10, anodeCathode=1, numDisp=1, abcdefgp={0,1,2,3,4,5,6,7,8}):
         """Initialized necessary variables for sevenSegShift class"""
       
         #creates new shift register object per display
         
-        for i in range(0, numDisp):
-            disp=new shiftRegister(ser, rclk, srclk)
+        dispList = [ ShiftRegister(ser, rclk, srclk) for i in range(numDisp)]
 
         self.a=abcdefg[0]
         self.b=abcdefg[1]
@@ -35,3 +37,6 @@ class sevenSegShift(object):
         else:
             self.ON=LOW
             self.OFF=HIGH
+
+    def setDigit(self, char, disp):
+        if disp<
