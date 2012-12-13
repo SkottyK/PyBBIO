2d7dcd52bc871dced60eb7dcc5414a19775d534a
loveatfirstbyte <skrulcik@gmail.com>
2012-12-13 10:50:00 -0500

    Update sevenSeg.py

--- sevenSeg.py
+++ sevenSeg.py
@@ -12,7 +12,20 @@ from shiftRegister import *
 
 class sevenSegShift(object):
 
+    #Defines input options to output specific characters
     
+    options = {0 : zero,
+        1 : one,
+        '1': one,
+        2: two,
+        '2': two,
+        3: three,
+        '3': three,
+        4: four,
+        '4': four,
+        
+
+}
 
 
     def _init_(self, ser=8, rclk=9, srclk=10, anodeCathode=1, numDisp=1, abcdefgp={0,1,2,3,4,5,6,7,8}):
@@ -20,7 +33,7 @@ class sevenSegShift(object):
       
         #creates new shift register object per display
         
-        dispList = [ ShiftRegister(ser, rclk, srclk) for i in range(numDisp)]
+        disp = new ShiftRegister(ser, rclk, srclk, numDisp)
 
         self.a=abcdefg[0]
         self.b=abcdefg[1]
@@ -38,10 +51,13 @@ class sevenSegShift(object):
             self.ON=LOW
             self.OFF=HIGH
 
-    def setDigit(self, char, disp):
-        if disp >= dispList.length(): disp=dispList.length()-1
-        dipList[disp].clearRegister()
-        switch char:
-            case 1:
-            case '1':
-                dispList[disp]
+    def setDigit(self, char, ndisp):
+        if ndisp >= self.disp.NUM_REGISTERS: ndisp=self.disp.NUM_REGISTERS-1
+        self.disp.clearRegisters()
+        option[char]()
+        self.disp.setPins(range(ndisp*8, ndisp*8+8), self.segments)
+
+
+    def one():
+        self.segments[]={0,1,1,0,0,0,0}
+    
